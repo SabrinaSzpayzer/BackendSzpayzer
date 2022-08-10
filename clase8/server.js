4532a8f26c23875434c3dd145135923ff30bf503
Sabrina Szpayzer <sabrinaszpayzer@yahoo.com.ar>
2022-08-09 22:08:13 -0300

    clase8

---
+++ clase8/server.js
@@ -0,0 +1,66 @@
+const express = require('express');
+
+const { Router } = express;
+
+const app = express();
+
+const routerProducts = new Router();
+routerProducts.use(express.json());
+routerProducts.use(express.urlencoded({ extended: true }));
+
+const Contenedor = require ('./Contenedor');
+
+const product = new Contenedor ('./productos.txt');
+
+routerProducts.get('/', async (req, res) => {
+    const allProducts = await product.getAll();
+    res.send(allProducts);
+})
+
+routerProducts.get('/:id', async (req, res) => {
+    const { id } = req.params;
+    const productById = await product.getById(id);
+    const allProducts = await product.getAll();
+    if ( id > allProducts.length ) {
+        res.send("Producto no encontrado");
+    } else {
+        res.send(productById);
+    }
+})
+
+routerProducts.post('/', async (req, res) => {
+    const newProduct = req.body;
+    const addProduct = await product.save(newProduct.title, newProduct.price, newProduct.thumbnail);
+    res.json(addProduct);
+})
+
+routerProducts.put('/:id', async (req, res) => {
+    const { id } = req.params;
+    const updatedProduct = req.body;
+    const putProduct = await product.putById(id, updatedProduct.title, updatedProduct.price, updatedProduct.thumbnail);
+    res.json(putProduct);
+})
+
+routerProducts.delete('/:id', async (req, res) => {
+    const { id } = req.params;
+    const deleteProductById = await product.deleteById(id);
+    const allProducts = await product.getAll();
+    if ( id > allProducts.length ) {
+        res.send("Producto no encontrado");
+    } else {
+        res.send(deleteProductById);
+    }
+})
+
+// Routers
+
+app.use('/api/productos', routerProducts);
+app.use(express.static('public'));
+
+// Server on
+
+const server = app.listen(8080, () => {
+    console.log(`Servidor http escuchando en el puerto ${server.address().port}`);
+})
+
+server.on ('error', error => console.log(`Error en servidor ${error}`));
