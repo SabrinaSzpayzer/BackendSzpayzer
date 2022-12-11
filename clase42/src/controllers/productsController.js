2571213574d6201669a8d993cffd97e4abaf4a4a
Sabrina Szpayzer <sabrinaszpayzer@yahoo.com.ar>
2022-12-11 18:34:03 -0300

    clase42

---
+++ clase42/src/controllers/productsController.js
@@ -0,0 +1,71 @@
+import {
+    getProductosTest,
+    getAllProducts,
+    getProductById,
+    postProduct,
+    putProductById,
+    deleteProductById,
+    deleteAll
+} from '../services/productsService.js'
+
+// Productos test
+
+async function productosTest (req, res) {
+    const allProducts = await getProductosTest()
+    if (allProducts.length > 0) {
+        res.render('main', {displayProduct: allProducts, listExists: true});
+    } else {
+        res.render('main', {listExists: false});
+    }
+}
+
+// Router products
+
+async function getAll (req, res) {
+    const allProducts = await getAllProducts();
+    if (allProducts.length > 0) {
+        res.render('main', {displayProduct: allProducts, listExists: true});
+    } else {
+        res.render('main', {listExists: false});
+    }
+}
+
+async function getById (req, res) {
+    const { id } = req.params;
+    const productById = await getProductById(id);
+    res.send(productById);
+}
+
+async function post (req, res) {
+    const newProduct = req.body;
+    const addProduct = await postProduct(newProduct.title, newProduct.price, newProduct.thumbnail);
+    res.send(newProduct);
+}
+
+async function putById (req, res) {
+    const { id } = req.params;
+    const updatedProduct = req.body;
+    const putProduct = await putProductById(id, updatedProduct.title, updatedProduct.price, updatedProduct.thumbnail);
+    res.send(updatedProduct);
+}
+
+async function deleteById (req, res) {
+    const { id } = req.params;
+    const deleteById = await deleteProductById(id);
+    res.send(deleteById);
+}
+
+async function deleteAllP (req, res) {
+    const deleteAllProd = await deleteAll();
+    res.send(deleteAllProd)
+}
+
+export {
+    productosTest,
+    getAll,
+    getById,
+    post,
+    putById,
+    deleteById,
+    deleteAllP
+}
---
+++ clase42/src/passport.js
@@ -0,0 +1,74 @@
+import User from './models.js'
+import passport from 'passport';
+import passportlocal from 'passport-local';
+const LocalStrategy = passportlocal.Strategy;
+import bCrypt from 'bcrypt';
+
+passport.use('signup', new LocalStrategy({
+    passReqToCallback: true
+},
+    (req, username, password, done) => {
+        User.findOne({ 'username': username }, (err, user) => {
+            if (err) {
+                return done(err);
+            };
+
+            if (user) {
+                return done(null, false);
+            }
+
+            const newUser = {
+                username: username,
+                password: createHash(password),
+                email: req.body.email,
+                firstName: req.body.firstName,
+                lastName: req.body.lastName
+            };
+
+            User.create(newUser, (err, userWithId) => {
+                if (err) {
+                    return done(err);
+                }
+                return done(null, userWithId);
+            })
+        });
+    }
+));
+
+passport.use('login', new LocalStrategy(
+    (username, password, done) => {
+        User.findOne({ username }, (err, user) => {
+            if (err) {
+                return done(err);
+            }
+
+            if (!user) {
+                return done(null, false);
+            }
+
+            if (!isValidPassword(user, password)) {
+                return done(null, false);
+            }
+
+            return done(null, user);
+        })
+    }
+));
+
+passport.serializeUser((user, done) => {
+    done(null, user._id);
+});
+
+passport.deserializeUser((id, done) => {
+    User.findById(id, done);
+});
+
+function createHash(password) {
+    return bCrypt.hashSync(password, bCrypt.genSaltSync(10), null);
+}
+
+function isValidPassword(user, password) {
+    return bCrypt.compareSync(password, user.password);
+}
+
+export default passport;
